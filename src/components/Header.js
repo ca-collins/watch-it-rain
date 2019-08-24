66668d1b124ac14c02f3c76b848f0058c1bb2e8e
krizk <cacollins08@gmail.com>
2019-08-24 16:31:41 -0400

    Add content / style to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledHeader = styled.div`
-
+  .logo {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 3rem;
+    text-align: center;
+    margin-bottom: 0.5rem;
+    margin-top: 1rem;
+  }
 `;
 
 const Header = () => (
   <StyledHeader>
-    Header
+    <h1 className="logo">Watch it rain</h1>
   </StyledHeader>
 );
 
