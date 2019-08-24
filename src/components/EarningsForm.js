99566044c22582414e428c200a3fa9a81a6f87aa
krizk <cacollins08@gmail.com>
2019-08-24 16:33:43 -0400

    Establish basic working form and useForm hook

--- src/components/EarningsForm.js
+++ src/components/EarningsForm.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import useForm from '../hooks/useForm';
 import styled from 'styled-components';
 
 const StyledEarningsForm = styled.div`
 
 `;
 
-const EarningsForm = () => (
-  <StyledEarningsForm>
-    EarningsForm
-  </StyledEarningsForm>
-);
+const EarningsForm = () => {
+  const submitForm = () => {
+    alert(`EarningsForm Submitted!
+           Pay Type: ${inputs.payType}
+           Pay Amount: ${inputs.payAmount}
+           Hours per Week: ${inputs.hoursPerWeek}`);
+}
+  const {inputs, handleInputChange, handleSubmit} = useForm(submitForm);
+
+  return (
+    <StyledEarningsForm>
+
+      <form className="init-form" onSubmit={handleSubmit}>
+
+        <h5>Pay Type</h5>
+        <div className="switch-wrapper pay-type-wrapper" onChange={handleInputChange}>
+          <label htmlFor="yearly-radio" id="yearly-label" className="radio-label">yearly</label>
+          <input type="radio" id="yearly-radio" name="payType" value="yearly" />
+          <label htmlFor="hourly-radio" id="hourly-label" className="radio-label">hourly</label>
+          <input type="radio" id="hourly-radio" name="payType" value="hourly"/>
+        </div>
+
+        <h5>Pay Amount</h5>
+        <input type="number" id="pay-amount" className="hourly-input yearly-input" placeholder="$ / yr" name="payAmount" onChange={handleInputChange} value={inputs.payAmount}/>
+
+        <h5>Hours per week</h5>
+        <input type="number" id="hrs-per-wk" className="yearly-input" placeholder="hrs / wk" name="hoursPerWeek" onChange={handleInputChange} value={inputs.hoursPerWeek}/>
+
+        <button type="submit">Make it rain!</button>
+
+      </form>
+
+    </StyledEarningsForm>
+  )
+};
 
 export default EarningsForm;
---
+++ src/hooks/useForm.js
@@ -0,0 +1,29 @@
+import {useState} from 'react';
+
+const useForm = (callback) => {
+  const [inputs, setInputs] = useState({
+    payType: '',
+    payAmount: '',
+    hoursPerWeek: ''
+  });
+
+  const handleSubmit = (e) => {
+    if (e) {
+      e.preventDefault();
+    }
+    callback();
+  }
+
+  const handleInputChange = (e) => {
+    e.persist();
+    setInputs(inputs => ({...inputs, [e.target.name]: e.target.value}));
+  }
+
+  return {
+    handleSubmit,
+    handleInputChange,
+    inputs
+  };
+}
+
+export default useForm;
